3eeace26fb3f790d36dec5c94442372c5ebf98ae
harshau007 <amanupadhyay2004@gmail.com>
2024-01-03 18:25:00 +0530

    Add credential true for CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { 
+    cors: {
+      credentials: true
+    }
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
