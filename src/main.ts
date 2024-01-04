5f166f5610797a2749c784f91e18015387b6f2d6
harshau007 <amanupadhyay2004@gmail.com>
2024-01-04 12:57:01 +0530

    Prod go brrrr.... part 2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:8080'], // Production Frontend URL
+    origin: ['https://house-frontend-theta.vercel.app'], // Production Frontend URL
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
    });
