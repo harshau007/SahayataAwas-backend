c83a2f72bd954501b3204dbab4741b26c75a5e5a
dhirajkalwar57 <dhirajkalwar57@gmail.com>
2024-01-04 20:26:09 +0530

    url update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://house-frontend-theta.vercel.app", // Production Frontend URL
+    origin: "https://house-frontend-six.vercel.app/", // Production Frontend URL
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
     exposedHeaders: ['Set-Cookie','Authorization'],
